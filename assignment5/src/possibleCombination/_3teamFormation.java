3cde5cb49a521406312d9e4bca81e04e2860eaf0
Taharina Tasnim <taharina.tasnim@stonybrook.edu>
2023-10-10 02:30:08 -0400

    modified ques 3 for ass5

--- assignment5/src/possibleCombination/_3teamFormation.java
+++ assignment5/src/possibleCombination/_3teamFormation.java
@@ -5,18 +5,19 @@ import java.util.*;
 
 public class _3teamFormation {
 	static void findKCombinations(char[] alphabets, int n, int k) {
-		List<Character> tmp = new ArrayList<>();
+		List<Character> tmp = new ArrayList<>();		// to store any combination of k
 		List<List<Character>> output = new ArrayList<>();
+		
 		nckHelper(alphabets, output, tmp, 0, k);
 		
-		for (List<Character> l:output) {
+		for (List<Character> l:output) {	// for printing
 			System.out.println(l);
 		}
 	}
 
 	static void nckHelper(char[] alphabets, List<List<Character>> output, List<Character> tmp, int index, int k) {
 		if (k == 0) {
-			output.add(new ArrayList<>(tmp));
+			output.add(new ArrayList<>(tmp));		// new = to make independent copies of the combinations
 			return;
 		}
 		
@@ -38,11 +39,19 @@ public class _3teamFormation {
 	}
 	
 	public static void main(String[] args) throws IOException {
-		char[] alphabets = {'A', 'B', 'C', 'D', 'E'};
+		char[] alphabets;
+		
 		System.out.println("group size: ");
     	int n = Integer.parseInt(getString());
+ 
+    	alphabets = new char[n];
+    	for (int i=0; i<n; i++) {
+    		alphabets[i] = (char)('A' + i);		// create robust char array i.e 3 = (A,B,C), 5 = (A,B,C,D,E)
+    	}
+ 
     	System.out.println("team size: ");
     	int k = Integer.parseInt(getString());
+    	
     	findKCombinations(alphabets, n, k);
 	}
 
